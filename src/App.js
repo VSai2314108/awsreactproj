6e56d7bffaf7f98c92343af16e8a8c8459888bcc
VijayaSai Somasundaram <somasundaramv@ufl.edu>
2022-02-03 19:03:28 -0500

    Comitting Test 1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import './App.css';
+import headshot from './headshot.jpg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to VijayaSai's Career Page
         </p>
+        <img className="photo" src={headshot}></img>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/VSai2314108"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Github
+        </a>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/vijayasai23/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Linkedin
         </a>
       </header>
     </div>
